8b5193057fac2332069086a7a13cc5ce3198762b
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-04-26 00:54:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+
 function destructivelyAppendKitten(name){
   kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.shift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destrutivelyRemoveFirstKitten() {
+  kittens.unshift()
   return kittens
 }
